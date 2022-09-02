16c0e25e672c0bc458cd435c044f0ebb387f8deb
javierch2022 <javierchavarria@tdf.edu.ar>
2022-09-01 21:01:20 -0300

    update

--- funciones/funcion.js
+++ funciones/funcion.js
@@ -1,6 +1,18 @@
-let btnCrear=document.querySelector("inputDatos");
 
-function enviarTexto(){
-   var mover=document.getElementById(inputDatos).value;
-   document.getElementById("inputTxt").value=texto
+////////////////////////////////////////////////////////
+
+const enviar = document.querySelector("#btnCrear");
+enviar.addEventListener("click", crearCajas);
+
+function crearCajas(){
+   const contenedor = document.getElementById("container");
+   contenedor.innerHTML="";
+   const input = document.querySelector("#inputDatos");
+   for (let i = 0; i < Number(input.value); i++){
+      contenedor.innerHTML += "<div class= 'caja'> " + i + "</div>"; 
+      }
+   input.value="";
 }
+
+
+// query selector siempre # de la id.
